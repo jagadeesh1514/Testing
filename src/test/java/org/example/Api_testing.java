8618b94b0362617d0137f6d6a5e8329f228e7bab
jagadeesh <jagadeeshmokasi@gmail.com>
2026-03-05 18:47:06 +0530

    commit in merge

--- src/test/java/org/example/Api_testing.java
+++ src/test/java/org/example/Api_testing.java
@@ -12,7 +12,7 @@ public class Api_testing{
          given().baseUri("https://dotesthere.com/api")
                 .header("Content-Type", "application/json")
                 .when().get("/users/1")
-                .then().statusCode(200).contentType().testhdclear;
+                .then().statusCode(200).contentType().testhdclear.main git add;
         System.out.println("Test");
 
     }
