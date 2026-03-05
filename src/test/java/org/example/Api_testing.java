e1ec488bc8ebc95c235500a191a9039ea99e48e7
jagadeesh <jagadeeshmokasi@gmail.com>
2026-03-05 18:13:10 +0530

    First commit

---
+++ src/test/java/org/example/Api_testing.java
@@ -0,0 +1,26 @@
+package org.example;
+
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+//rufhsdyufgusydfguagyuyft
+
+public class Api_testing{
+
+    public static void test(){
+         given().baseUri("https://dotesthere.com/api")
+                .header("Content-Type", "application/json")
+                .when().get("/users/1")
+                .then().statusCode(200);
+        System.out.println("Test");
+
+    }
+
+    public static void main(String args[]){
+
+      Api_testing.test();
+
+    }
+
+}
